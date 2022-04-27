58af4350262577d36ec9c5637999530ef94818ff
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 22:22:08 +0800

    Delete scripts directory

--- scripts/build-travis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -----------------------------------------------------
-# set strict mode: fail smart, don't hide pipe errors
-set -euo pipefail
-IFS=$'\n\t'
-# -----------------------------------------------------
-
-npm run build && npm run test:unit
